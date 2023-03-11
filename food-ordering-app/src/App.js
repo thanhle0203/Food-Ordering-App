5e4cb7dade6faa1b2e655d6d2174ef47087ced40
Thanh Le <thanhlevan0203@gmail.com>
2023-03-11 14:42:38 -0800

    navigation file

--- food-ordering-app/src/App.js
+++ food-ordering-app/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import { Header } from "./components/Header"
+import Navigation from './navigation';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navigation />
     </div>
   );
 }
