9b3ebf6960375604998d9aa301b281b0abbd88c3
Thanh Le <thanhlevan0203@gmail.com>
2023-03-11 13:46:57 -0800

    create components and navigation

---
+++ food-ordering-app/src/navigation/index.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "../components/Header";
+
+const Navigation = () => {
+    return (
+        <BrowserRouter>
+            <Header />
+        </BrowserRouter>
+    )
+}
+
+export default Navigation;
